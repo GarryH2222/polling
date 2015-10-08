3d58d5e7508a7288359583afe90d7430ff3dc546
tehfl4n <hwanggarry@gmail.com>
2015-10-08 10:21:00 -1000

    final polling
    final ver of polling

--- polling/collections/polls.js
+++ polling/collections/polls.js
@@ -2,3 +2,20 @@
  * Created by omgitsg on 10/1/15.
  */
 Polls = new Mongo.Collection('polls');
+
+if (Meteor.isServer) {
+
+  // This code only runs on the server
+
+  Meteor.publish("polls", function () {
+
+    return Polls.find();
+
+  });
+
+}
+
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Meteor.subscribe("polls");
+}
